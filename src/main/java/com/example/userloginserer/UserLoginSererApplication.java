4ed7bb6d20d408b642f82a041adc4c0033b38717
yun <hsyyun1110@163.com>
2024-11-01 20:05:03 +0800

    Initial commit

---
+++ src/main/java/com/example/userloginserer/UserLoginSererApplication.java
@@ -0,0 +1,13 @@
+package com.example.userloginserer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginSererApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginSererApplication.class, args);
+    }
+
+}
